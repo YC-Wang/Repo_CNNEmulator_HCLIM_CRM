da082c2fed92716d4810269789fa181e62d9125d
Yi-Chi Wang <yichi.sinica@gmail.com>
2026-08-26 15:05:10 +0200

    test exp1

--- submit_script.sh
+++ submit_script.sh
@@ -5,10 +5,5 @@
 
 module load Mambaforge/23.3.1-1-hpc1
 mamba activate high_res_env
-#python -u training_baseline_models.py
-#python -u training_ncp_mse.py
-#python -u training_ncp_mse_2003to2009.py
-python -u scripts/training_ncp_mse.py exp1_normal_withT.yaml
-#python -u training_ncp_mse_normal2009_nosm.py
-#python -u inference_ncp_mse.py
-#python -u inference_ncp_mse_normalized_mrsol_normal2009_nosm.py
+#python -u scripts/training_ncp_mse.py exp1_normal_withT.yaml
+python scripts/inference_ncp_mse.py exp1_normal_withT.yaml --model-file /nobackup/rossby27/users/sm_yicwa/PROJECTS/01-PROJ_emulator/01-rampal2021-unet/output/models/20260825-1718_cnn_tas_exp1_heatwave_withT.h5
